9c48ee42c6caec8541aa6d7cafae496d5d448009
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2020-11-30 14:50:21 +0100

    Start es 3

---
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,62 @@
+package it.unibo.oop.lab.workers02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    private final int nthread;
+
+    public MultiThreadedSumMatrix(final int nthread) {
+        this.nthread = nthread;
+    }
+
+    private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private long res;
+
+        /**
+         * Build a new worker.
+         * 
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final double[][] matrix, final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
+            /*for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
+                this.res += this.list.get(i);
+            }*/
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         * 
+         * @return the sum of every element in the array
+         */
+        public long getResult() {
+            return this.res;
+        }
+
+    }
+
+    public double sum(final double[][] matrix) {
+
+        System.out.println(matrix[0].length);
+        return 0;
+    }
+
+}
